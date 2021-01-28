8bc1bc9f90a78410b8f5058cb87722b66c2a496e
hellbrandt <hellbrandt@googlemail.com>
2021-01-28 11:33:36 +0000

    Add beginning of Burger Builder Container 125

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Auxilliary';
+import classes from './Layout.module.css';
+
 
 const layout = (props) => (
   <Aux>
     <div>Toolbar, Sidedrawer, Backdrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
