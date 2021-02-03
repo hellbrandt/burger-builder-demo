86485e139895589cc1e3e764341bab634d1c9566
hellbrandt <hellbrandt@googlemail.com>
2021-02-03 12:15:16 +0000

    Improve performance on Modal 154

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Auxilliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}
-    >
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
+  componentDidUpdate() {
+    console.log('[Modal] did update');
+  }
 
-export default modal; 
+  shouldComponentUpdate(nextProps, nextState) {
+    // Could in theory check for more props or use PureComponent which checks all
+    // But that doesn't give any benefit here and would take more resource
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    )
+  }
+}
+
+export default Modal;
