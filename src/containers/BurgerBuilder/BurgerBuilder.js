24d78ce5200a0bf6afc278cbe0a2c7c92be97dfb
hellbrandt <hellbrandt@googlemail.com>
2021-02-05 17:22:46 +0000

    Add Axios instance & send POST req to Firebase 181

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-order';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -77,7 +78,24 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue!');
+    //alert('You continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice, // would recalculate price on server
+      customer: {
+        name: 'Neil Benson',
+        address: {
+          street: 'test street',
+          postCode: 'L12 345',
+          country: 'United Kingdom'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
+    }
+    axios.post('/orders.json', order) // .json is required by Firebase
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
 
   render() {
