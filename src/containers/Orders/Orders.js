de4c9fbe4cf46afe2875dc0a954318dcd9fca504
hellbrandt <hellbrandt@googlemail.com>
2021-02-23 16:31:56 +0000

    Fetch orders and push onto array 230

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+  state = {
+    order: [],
+    loading: true
+  }
+  componentDidMount() {
+    axios.get('/orders.json')
+      .then(response => {
+        const fetchedOrders = [];
+        for (let key in response.data) {
+          fetchedOrders.push({
+            ...response.data[key],
+            id: key
+          });
+        }
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch(errors => {
+        this.setState({ loading: false });
+      })
+  }
+
   render() {
     return (
       <div>
@@ -14,4 +37,4 @@ class Orders extends Component {
 
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
